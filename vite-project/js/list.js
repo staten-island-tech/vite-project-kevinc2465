54814bc902fefef76335c2d20706890df61bf328
kevinc2465 <chenk5104@gmail.com>
2022-12-08 13:15:50 -0500

    started list of element

---
+++ vite-project/js/list.js
@@ -0,0 +1,28 @@
+const fruits = {
+  results: [
+    {
+      name: apple,
+      color: red,
+      price: "$1.32",
+      flavor: "sweet",
+      img_url:
+        "https://healthjade.com/wp-content/uploads/2017/10/apple-fruit.jpg",
+    },
+    {
+      name: lemon,
+      color: yellow,
+      price: "$0.53",
+      img_url:
+        "https://www.shutterstock.com/image-photo/lemon-fruit-leaf-isolate-whole-260nw-1727353507.jpg",
+      flavor: "sour",
+    },
+    {
+      name: orange,
+      color: orange,
+      price: "$1.58",
+      img_url:
+        "https://healthjade.com/wp-content/uploads/2017/10/orange-fruit.jpg",
+      flavor: "sweet",
+    },
+  ],
+};
